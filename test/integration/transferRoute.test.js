5e8e69e7cabb07dbd7355ab03082a71958085586
dhamolahedonist <dhamolahedonist>
2023-07-29 17:08:31 +0100

    created a unit test for creating bulk transfer

--- test/integration/transferRoute.test.js
+++ test/integration/transferRoute.test.js
@@ -9,7 +9,7 @@ const transferModel = require("../../models/transferModel");
 const receiptModel = require("../../models/recipientModel");
 
 const header = {
-  Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7Il9pZCI6IjY0YzUwN2JhNDE3N2FkYTAzMjM3N2M3OSIsImVtYWlsIjoib2R1YmFzYW11ZWw2NkBnbWFpbC5jb20ifSwiaWF0IjoxNjkwNjQzMTI0LCJleHAiOjE2OTA2NDY3MjR9.iwpSC7YimldifIU64TmRkKtMlDxz9xGxgqZJOpckf78`,
+  Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7Il9pZCI6IjY0YzUwN2JhNDE3N2FkYTAzMjM3N2M3OSIsImVtYWlsIjoib2R1YmFzYW11ZWw2NkBnbWFpbC5jb20ifSwiaWF0IjoxNjkwNjQ2ODExLCJleHAiOjE2OTA2NTA0MTF9.jiGIb4N4SChfFqbHCs3HK16HSRyw_d1RxAi36kvfz_I`,
 };
 
 const user_id = "64c507ba4177ada032377c79";
@@ -37,6 +37,47 @@ describe("Initiate a transfer", () => {
           amount: 500,
           narration: "Akhlm Pstmn Trnsfr xx007",
           currency: "USD",
+        })
+        .end((err, response) => {
+          response.should.have.status(200);
+          response.body.should.be.a("object");
+          response.body.should.have.property("success");
+          response.body.should.have.property("message");
+          response.body.should.have.property("data");
+          resolve();
+        });
+    });
+  });
+
+  it("create a bulk-transfer", async () => {
+    return new Promise(async (resolve) => {
+      await cleanup();
+
+      chai
+        .request(app)
+        .post("/api/v1/bulk-transfers")
+        .set(header)
+        .send({
+          recipients: [
+            {
+              first_name: "John",
+              last_name: "Doe",
+              email: "john.doe@example.com",
+              mobile_number: "1234567890",
+              recipient_address: "123 Main St",
+              amount: 1200,
+              currency: "USD",
+            },
+            {
+              first_name: "Jane",
+              last_name: "Smith",
+              email: "jane.smith@example.com",
+              mobile_number: "9876543210",
+              recipient_address: "456 Elm St",
+              amount: 10000,
+              currency: "NGN",
+            },
+          ],
         })
         .end((err, response) => {
           response.should.have.status(200);
